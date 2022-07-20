643a03c2bc14179f9085d601a7627cd6d5393122
elenilson <sousalele99@icloud.com>
2022-07-20 09:57:33 -0300

    implementacão do put delete usuario

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -25,20 +25,25 @@ export class EntrarComponent implements OnInit {
   }
 
   entrar(){
-    this.auth.entrar(this.usuarioLogin).subscribe((resp: UsuarioLogin)=>{
-      this.usuarioLogin = resp
+    this.auth.entrar(this.usuarioLogin).subscribe(
+      (resp: UsuarioLogin) => {
+        this.usuarioLogin = resp;
 
-      environment.foto = this.usuarioLogin.foto
-      environment.id = this.usuarioLogin.id
-      environment.nome =  this.usuarioLogin.nome
-      environment.token = this.usuarioLogin.token
+        environment.id = this.usuarioLogin.id;
+        environment.token = this.usuarioLogin.token;
+        environment.nome = this.usuarioLogin.nome;
+        environment.tipo = this.usuarioLogin.tipo;
+        environment.foto = this.usuarioLogin.foto;
 
-      this.router.navigate(['/inicio'])
-    }, erro =>{
-      if(erro.status == 500){
-     alert('Usuário ou senha estão incorretos!')
+
+        this.router.navigate(['/inicio']);
+      },
+      (erro) => {
+        if (erro.status == 401) {
+          alert('Usuário ou senha estão incorretos!');
+        }
       }
-    })
+    );
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -20,15 +20,19 @@ export class AuthService {
   ) { }
 
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
-    return this.http.post<UsuarioLogin>('http://localhost:8080/usuarios/logar', usuarioLogin)
+    return this.http.post<UsuarioLogin>('https://bplelesousa.herokuapp.com/usuarios/logar', usuarioLogin)
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
-    return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
+    return this.http.post<Usuario>('https://bplelesousa.herokuapp.com/usuarios/cadastrar', usuario)
+  }
+
+  atualizar(usuario: Usuario): Observable<Usuario>{
+    return this.http.post<Usuario>('https://bplelesousa.herokuapp.com/usuarios/atualizar', usuario)
   }
 
   getByIdUsuario(id: number):Observable<Usuario>{
-    return this.http.get<Usuario>(`http://localhost:8080/usuarios/${id}`)
+    return this.http.get<Usuario>(`https://bplelesousa.herokuapp.com/usuarios/${id}`)
   }
 
   logado(){
